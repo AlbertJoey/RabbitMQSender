bc310da8bc1d2a6a093cc2770c650b233a14db6b
AlbertJoey <cheng.jun@Outlook.com>
2016-05-27 11:41:55 +0800

    update commit

--- testRabbitMQSender/Program.cs
+++ testRabbitMQSender/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
 using System.Threading;
+using RabbitMQ.Client.Events;
 
 namespace testRabbitMQSender
 {
@@ -144,53 +145,97 @@ namespace testRabbitMQSender
             #endregion
 
             #region Topics
-            var factory = new ConnectionFactory() { HostName = "192.168.100.37", Password = "Git123!@#", UserName = "GitAdmin", Port = 5672 };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                for (int i=0; i < 30; i++)
-                {
+            //var factory = new ConnectionFactory() { HostName = "192.168.100.37", Password = "Git123!@#", UserName = "GitAdmin", Port = 5672 };
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    for (int i=0; i < 8; i++)
+            //    {
 
-                    var a = new Random();
-                    var severity="";
-                    var message ="";
-                    if (a.Next(9) % 2 == 0)
-                    {
-                        severity = "warning";
-                        message = string.Format("this is warning ,time :{0}", i);
-                    }
-                    else if (a.Next(10) % 3 == 0)
-                    {
-                        severity = "info";
-                        message = string.Format("this is warning ,time :{0}", i);
-                    }
-                    else
-                    {
-                        severity = "error";
-                        message = string.Format("this is error ,time :{0}", i);
-                    }
+            //        var a = new Random();
+            //        var severity="";
+            //        var message ="";
+            //        var serverityList = new string[] { "quick.orange.rabbit", "lazy.orange.elephant", "lazy.brown.fox", "lazy.pink.rabbit", "quick.brown.fox", "quick.orange.fox", "quick.orange.male.rabbit", "lazy.orange.male.rabbit" };
+            //        if (i <= 7) severity = serverityList[i];
+            //        else severity = serverityList[a.Next(7)];
+            //        message = string.Format(" time {0}", i);
+            //        channel.ExchangeDeclare(exchange: "topic_logs",
+            //                                type: "topic");
+            //        var body = Encoding.UTF8.GetBytes(message);
+            //        channel.BasicPublish(exchange: "topic_logs",
+            //                             routingKey: severity,
+            //                             basicProperties: null,
+            //                             body: body);
+            //        Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+            //        Thread.Sleep(2000);
+            //    }
+            //    Console.WriteLine(" Press [enter] to exit.");
+            //    Console.ReadLine();
+            //} 
+            #endregion
 
-                    channel.ExchangeDeclare(exchange: "topic_logs",
-                                            type: "topic");
+            #region RPC
+            var rpcClient = new RPCClient();
 
-                    //var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
-                    //var message = (args.Length > 1)
-                    //              ? string.Join(" ", args.Skip(1).ToArray())
-                    //              : "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
-                    channel.BasicPublish(exchange: "topic_logs",
-                                         routingKey: severity,
-                                         basicProperties: null,
-                                         body: body);
-                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
-                }
-           
-            } 
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+            Console.WriteLine(" [.] Got '{0}'", response);
+
+            rpcClient.Close();
+            Console.ReadLine();
             #endregion
         }
         private static string GetMessage(string[] args)
         {
             return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+        
+    }
+    public class RPCClient
+    {
+        private IConnection connection;
+        private IModel channel;
+        private string replyQueueName;
+        private QueueingBasicConsumer consumer;
+
+        public RPCClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.100.37", Password = "Git123!@#", UserName = "GitAdmin", Port = 5672 };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queue: replyQueueName,
+                                 noAck: true,
+                                 consumer: consumer);
+        }
+
+        public string Call(string message)
+        {
+            var corrId = Guid.NewGuid().ToString();
+            var props = channel.CreateBasicProperties();
+            props.ReplyTo = replyQueueName;
+            props.CorrelationId = corrId;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "rpc_queue",
+                                 basicProperties: props,
+                                 body: messageBytes);
+
+            while (true)
+            {
+                var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                if (ea.BasicProperties.CorrelationId == corrId)
+                {
+                    return Encoding.UTF8.GetString(ea.Body);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            connection.Close();
         }
     }
 }
